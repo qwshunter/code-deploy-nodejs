507a2358fe27dfe965a980d986dd59524faebbcb
Rahul Srivastava <qwsrahulsrivastava@gmail.com>
2020-08-14 13:33:09 +0530

    xxx

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', async (req, res) => {
+	console.log("FInal healthCheck ===> ");
+   res.send('Hello World from viren piranha ')
+})
+
+app.get('/healthCheck', async (req, res) => {
+	console.log("FInal healthCheck ===> ");
+   res.send('Hello World from viren piranha healthCheck ')
+})
+
+const port = 8999;
+	app.listen(port, () => console.log(`Listening on port ..vifen`));
